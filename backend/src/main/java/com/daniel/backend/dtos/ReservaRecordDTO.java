367d036aeaf6aba17dc4738929f65d7e9551f859
danielfreitassc <danielfreitassc@gmail.com>
2024-03-30 00:04:56 -0300

    Arrumado as regras de negocios

--- backend/src/main/java/com/daniel/backend/dtos/ReservaRecordDTO.java
+++ backend/src/main/java/com/daniel/backend/dtos/ReservaRecordDTO.java
@@ -5,6 +5,6 @@ import java.time.LocalDateTime;
 import com.daniel.backend.models.MotoristaEntity;
 import com.daniel.backend.models.VeiculoEntity;
 
-public record ReservaRecordDTO(LocalDateTime reservado, LocalDateTime saida, LocalDateTime chegada, VeiculoEntity veiculo, MotoristaEntity motoristaEntity, String organograma, String finalidade, String observacoes) {
+public record ReservaRecordDTO(LocalDateTime reservado, LocalDateTime saida, LocalDateTime chegada, VeiculoEntity veiculo, MotoristaEntity motorista, String organograma, String finalidade, String observacoes) {
     
 }
